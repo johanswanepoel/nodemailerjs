d396ffc5ca12bb240e5c0769a815bb2acaa3260e
Johan Swanepoel <johan.swanepoel@hearxgroup.com>
2019-09-30 15:43:17 +0200

    CHORE update dependencies

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const nodemailer = require('nodemailer');
 
 const htmlSource = `
@@ -8,47 +7,74 @@ const htmlSource = `
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Nodemailer Example</title>
 </head>
 <body style="text-align:center;">
+    <p>An example implementation of the nodemailer package to send images using CIDs</p>
 	<h1>This is an image of Stellenbosch:</h1>
 	<img src="cid:anyt3xTy0uw@ntgoeshere" width="250" height="250" />
 </body>
-</html>`
+</html>`;
 
 // enter your own details here
-var mailOptions = {
-	from: 'noreply',
-	to: 'recipient.name@email.net',
-    subject: 'Embedded image Test ' + Date(),
-    text: 'Fallback message goes here',
-    html: htmlSource,
-    attachments: [{
-        filename: 'Vue_du_vignoble_de_Stellenbosch.jpg',
-        path: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Vue_du_vignoble_de_Stellenbosch.jpg',
-        cid: 'anyt3xTy0uw@ntgoeshere' // same cid value as in the html img src
-    }]
+const mailOptions = {
+  from: 'noreply',
+  to: 'johan.swanepoel@hearxgroup.com',
+  subject: 'Embedded image Test ' + Date(),
+  text: 'Fallback message goes here',
+  html: htmlSource,
+  attachments: [
+    {
+      filename: 'Vue_du_vignoble_de_Stellenbosch.jpg',
+      path:
+        'https://upload.wikimedia.org/wikipedia/commons/6/6b/Vue_du_vignoble_de_Stellenbosch.jpg',
+      cid: 'anyt3xTy0uw@ntgoeshere', // same cid value as in the html img src
+    },
+  ],
 };
 
 // enter your own details here
-var smtpConfig = {
-    host: 'smtp.yourmail.net',
-    port: 465,
-    secure: true, // use SSL
-    auth: {
-        user: 'yourusername',
-        pass: 'yourpassword'
-    }
+const smtpConfig = {
+  host: 'smtp.gmail.com', // replace with yours
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+    user: 'johans.jurie@gmail.com',
+    pass: 'Hierdieismygooglewagwoord.1',
+  },
 };
 
-let transporter = nodemailer.createTransport(smtpConfig);
+async function getFakeSmtp() {
+    const testAccount = await nodemailer.createTestAccount();
+    const fakeSmtp = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass // generated ethereal password
+        }
+    };
+    
+    return fakeSmtp;
+}
 
-transporter.sendMail(mailOptions, (err) => {
+async function send(options, smtp) {
+/* Only needed if you don't have a real mail account for testing */
+    const fakeSmtp = await getFakeSmtp();
+    // smtp = fakeSmtp;
+
+  let transporter = nodemailer.createTransport(smtp);
+
+  transporter.sendMail(options, err => {
     if (err) {
-        console.info('Error sending message')
-        console.warn('' + err);
-    } else {
-        console.error('Message sent')
+      return console.error('Error sending message', err);
     }
-});
+    
+      console.log('Message sent!');
+    
+  });
+}
 
+send(mailOptions, smtpConfig);
+   
